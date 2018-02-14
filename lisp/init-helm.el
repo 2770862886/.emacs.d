43c5ee2a3abfc2676315b20ef5faf45240838112
liangchao.lc <liangchao.lc@alibaba-inc.com>
2018-02-14 14:37:37 +0800

    enable helm-find-files keybinding

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -11,6 +11,9 @@
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
+(global-unset-key (kbd "C-x C-f"))
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
 (provide 'init-helm)
 
 ;;; init-helm.el ends here
