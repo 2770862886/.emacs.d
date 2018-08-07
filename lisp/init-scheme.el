d7bb1f47321ddad688a60bff4a7f067ed919bf33
liangchao <liangchao.lc@alibaba-inc.com>
2018-08-07 14:36:15 +0800

    use racket-mode as the scheme dev tools

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -1,13 +1,26 @@
-;;; init-scheme --- use geiser, company-mode, paredit
+;;; init-scheme --- use racket-mode, paredit for development env
 ;;; Commentary:
 ;;; Code:
 
-(require-package 'geiser)
+(require-package 'racket-mode)
 
-(setq scheme-program-name "racket")
-(setq geiser-active-implementations '(racket))
-(setq geiser-repl-history-filename "~/.emacs.d/geiser-history")
-(setq geiser-repl-query-on-kill-p nil)
+(setq racket-racket-program "racket")
+(setq racket-raco-program "raco")
+
+(add-to-list 'auto-mode-alist '("\\.scm\\'" . racket-mode))
+
+(add-hook 'racket-mode-hook
+          (lambda ()
+            (define-key racket-mode-map (kbd "C-c r") 'racket-run)
+            (paredit-mode 1)))
+
+;; unicode input method
+(add-hook 'racket-mode-hook #'racket-unicode-input-method-enable)
+(add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
+
+(setq tab-always-indent 'complete)
+
+;; (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (provide 'init-scheme)
 ;;; init-scheme ends here
