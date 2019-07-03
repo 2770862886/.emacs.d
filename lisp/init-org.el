f10a4fffb6922b1078e26f18dedf504ae8ce4389
liangchao <liangchao.lc@alibaba-inc.com>
2019-07-03 18:54:29 +0800

    temp save

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -124,7 +124,7 @@ typical word processor."
     (when (fboundp 'writeroom-mode)
       (writeroom-mode 0))))
 
-;;(add-hook 'org-mode-hook 'buffer-face-mode)
+(add-hook 'org-mode-hook 'buffer-face-mode)
 
 
 (setq org-support-shift-select t)
@@ -403,7 +403,47 @@ typical word processor."
      (sql . t)
      (sqlite . t))))
 
+;; #### Better Appearance
+
+(font-lock-add-keywords 'org-mode
+                        '(("^ +\\([-*]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(require-package 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(add-hook 'org-mode-hook 'visual-line-mode)
+
+(custom-theme-set-faces
+ 'user
+ '(org-block                 ((t (:inherit fixed-pitch))))
+ '(org-document-info         ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-link                  ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line             ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value        ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword       ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-tag                   ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim              ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent                ((t (:inherit (org-hide fixed-pitch))))))
+
+(require-package 'pomodoro)
+;;(pomodoro-add-to-mode-line)
+(setq pomodoro-break-time 2)
+(setq pomodoro-long-break-time 5)
+(setq pomodoro-work-time 25)
+(setq-default mode-line-format
+              (cons '(pomodoro-mode-line-string pomodoro-mode-line-string)
+                    mode-line-format))
+
+;; ####
+
 ;; #### Add global function for org interactive function
+
+(defun inbox ()
+  "Used to open inbox org file."
+  (interactive)
+  (find-file "~/CloudStation/Org/inbox.org"))
+
 ;; by liangchao, 2018.2.28
 (defun todo ()
   "Used to open todos org file."
