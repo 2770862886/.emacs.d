64ed32bdb354b960432e22d563a2a11acacd3998
liangchao <liangchao.lc@alibaba-inc.com>
2019-07-08 11:52:51 +0800

    add theme of dracula

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -19,8 +19,11 @@
   (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
   (prefer-coding-system 'utf-8))
 
+;; #### config default font for displaying org-ellipsis
+;; by liangchao, 2019.7.5
 (when *is-a-mac*
   (set-default-font "Dejavu Sans Mono for Powerline"))
+;; ####
 
 (provide 'init-locales)
 ;;; init-locales.el ends here
