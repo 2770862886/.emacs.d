04e629d05e9d28611673941b2ac6eca47accb670
liangchao <liangchao.lc@alibaba-inc.com>
2020-05-09 15:03:10 +0800

    manage plantuml config

--- lisp/init-plantuml.el
+++ lisp/init-plantuml.el
@@ -6,16 +6,18 @@
 
 (use-package plantuml-mode
   :ensure t
+  :mode "\\.plantuml$"
   :config
-  ;; Enable plantuml-mode for PlantUML files
-  (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
   ;; Sample jar configuration
   (setq plantuml-jar-path "~/.emacs.d/bin/plantuml.jar")
   (setq plantuml-default-exec-mode 'jar)
-  (setq plantuml-output-type "png")
-  (with-eval-after-load 'flycheck
-    (require-package 'flycheck-plantuml)
-    (flycheck-plantuml-setup)))
+  (setq plantuml-output-type "png"))
+
+(use-package flycheck-plantuml
+  :ensure t
+  :requires plantuml-mode
+  :config
+  (flycheck-plantuml-setup))
 
 (provide 'init-plantuml)
 ;;; init-plantuml.el ends here
